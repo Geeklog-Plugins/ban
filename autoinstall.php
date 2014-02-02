c2384c1be6dd34f637b02e45ac241804c5fc82ac
Tom <websitemaster@cogeco.net>
2014-02-01 19:29:11 -0500

    Added Ban plugin to Tools Command & Control Group

--- autoinstall.php
+++ autoinstall.php
@@ -51,7 +51,7 @@ function plugin_autoinstall_ban($pi_name)
     $info = array(
         'pi_name'         => $pi_name,
         'pi_display_name' => $pi_display_name,
-        'pi_version'      => '2.0.0',
+        'pi_version'      => '2.1.0',
         'pi_gl_version'   => '1.8.0',
         'pi_homepage'     => 'http://http://code.google.com/p/geeklog/'
     );
