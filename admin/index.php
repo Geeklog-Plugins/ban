06a42ec35794dc5ba1e6b389b935c8a8159b49a8
eSilverStrike <eSilverStrike@users.noreply.github.com>
2022-05-05 11:47:08 -0400

    Fixed Timezone issue with downloading SFS database

--- admin/index.php
+++ admin/index.php
@@ -242,6 +242,7 @@ function ban_list()
 
             // Manual Retry date
             $now = DateTime::createFromFormat('U.u', microtime(true));
+			$now->setTimeZone(new DateTimeZone(TimeZoneConfig::getTimezone()));
             $current_date = $now->format("Y-m-d H:i:s");
 
             if ($sfs_last_download_date == '') {
